7fe1ab9ee70a8588fe6cdc48d09f2ca71036f31d
Ana Camilly Guedes <90116902+anacamilly@users.noreply.github.com>
2022-04-25 10:45:46 -0300

    Update queries.py

--- queries.py
+++ queries.py
@@ -3,4 +3,4 @@ def ler_todos_usuarios(bd):
     return ler_bd(bd, "SELECT * FROM Usuario")
 def ler_usuario_nome(bd, nome):
     query = "SELECT * FROM Usuario WHERE nome LIKE ?"
-    return ler_bd(bd, query, ('%'+nome+'%',))
+    return ler_bd(bd, query, ('%nome%',))
